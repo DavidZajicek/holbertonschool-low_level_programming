683d45a2304b799a9680da687f4fc7dd7ff44684
David Zajicek <david.a.zajicek@outlook.com>
2024-06-18 03:12:06 +0000

    perror again please

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -7,8 +7,6 @@
  */
 int main(void)
 {
-	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
-	fwrite(str, sizeof(char), strlen(str), stderr);
+	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
