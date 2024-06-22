fe8407fcaba769a9f003df1f4261718d9735cd3b
David Zajicek <david.a.zajicek@outlook.com>
2024-06-22 02:55:18 +0000

    oh right, numbers have multiple digits above 9

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,17 +7,31 @@
  */
 int times_table(void)
 {
-	int i, j;
+	int i, j, k;
 
 	for (i = 0 ; i <= 9 ; i++)
 	{
 		for (j = 0 ; j <= 9 ; j++)
 		{
-			_putchar('0' + (i * j));
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-		}
+			result = i * j;
+
+
+			if (result < 10)
+			{
+				if (j > 0)
+					_putchar(' ');
+				_putchar(result + '0');
+			}
+			else
+			{
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+			}
+			if (j < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		_putchar('\n');
 	}
 
