5caf22df0f1bd0f5446c00ad3ebede45989e798d
David Zajicek <david.a.zajicek@outlook.com>
2024-06-22 01:20:19 +0000

    2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10 times, each on a new line
+ * Return: 0
+ */
+int print_alphabet_x10(void)
+{
+	char ch;
+
+	for (int i = 0; i == 9; i++)
+	{
+		for (ch = 'a' ; ch <= 'z' ; ch++)
+		{
+			_putchar(ch);
+		}
+	}
+	_putchar('\n');
+	return (0);
+}
--- functions_nested_loops/README.md
+++ functions_nested_loops/README.md
@@ -2,3 +2,4 @@
 
 0-putchar.c - Print _putchar using _putchar
 1-alphabet.c - print the alphabet in lower case
+2-print_alphabet_x10.c - print the alphabet 10 times
