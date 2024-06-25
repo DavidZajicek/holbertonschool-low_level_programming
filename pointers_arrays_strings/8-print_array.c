4b69d7ae11234c591fa22ffd1cf0097b84b97c89
David Zajicek <david.a.zajicek@outlook.com>
2024-06-25 11:08:25 +0000

    but the output was correct...

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -13,9 +13,10 @@ int print_array(int *a, int n)
 
 	for (i = 0 ; i < n ; i++)
 	{
-		printf("%d", *(a + i));
 		if (i != (n - 1))
-			printf(", ");
+			printf("%d, ", *(a + i));
+		else
+			printf("%d", *(a + i));
 	}
 	_putchar('\n');
 	return (0);
