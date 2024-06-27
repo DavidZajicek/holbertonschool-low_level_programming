1f93a7ca3f76ff4b2e9394753f62fa646935441c
David Zajicek <david.a.zajicek@outlook.com>
2024-06-27 23:47:45 +0000

    the dangers of copy paste from your old code hehe

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -4,14 +4,14 @@
  * @s2: second string
  * Return: 0 if the same, the number if different
  */
-char *_strcmp(char *s1, char *s2)
+int *_strcmp(char *s1, char *s2)
 {
 	int total = 0;
 
 	while (*s1 != '\0' && *s2 != '\0')
 	{
 		total += *s1 - *s2;
-		if (*dest != '\0')
+		if (*s1 != '\0')
 			s1++;
 		if (*s2 != '\0')
 			s2++;
