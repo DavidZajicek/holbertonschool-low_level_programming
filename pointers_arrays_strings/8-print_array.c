24d4ca5a43897d85490cbd79f6c4dfbeee2223b4
David Zajicek <david.a.zajicek@outlook.com>
2024-06-27 04:11:44 +0000

    wait, why did I do that

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -16,9 +16,7 @@ int print_array(int *a, int n)
 		if (i != (n - 1))
 			printf("%d, ", *(a + i));
 		else
-			printf("%d", *(a + i));
+			printf("%d\n", *(a + i));
 	}
-	_putchar('\n');
-	_putchar('\n');
 	return (0);
 }
