e8c09e1e0c4f3c4b7e78e3644999fc56f8e1b761
David Zajicek <david.a.zajicek@outlook.com>
2024-07-01 03:12:39 +0000

    5-strstr.c

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * _strstr - locate a substring
+ * @haystack: string to look inside
+ * @needle: substring to find
+ * Return: address of the start of the needle if found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *result = NULL, *marker;
+	int i = 0, found = 0;
+
+	while (*haystack != '\0')
+	{
+		if (*haystack == *needle)
+		{
+			marker = haystack;
+			i = 0;
+			while (needle[i] != '\0' && haystack[i] != '\0')
+			{
+				if (haystack[i] != needle[i])
+				{
+					found = 0;
+				}
+				else
+				{
+					found = 1;
+					i++;
+				}
+			}
+			if (found == 1)
+			{
+				result = marker;
+				break;
+			}
+		}
+		haystack++;
+	}
+	return (result);
+}
