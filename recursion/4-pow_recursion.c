90f3e2127988d886f0b6690d303371827d66eedc
David Zajicek <david.a.zajicek@outlook.com>
2024-07-03 02:57:10 +0000

    pow 0 is 1, didn't know

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -8,9 +8,11 @@ int _pow_recursion(int x, int y)
 {
 	if (x == 0)
 		return (0);
+	if (y == 0)
+		return (1);
 	if (y < 0)
 		return (-1);
-	if (y == 0 || y == 1)
+	if (y == 1)
 		return (x);
 	return (x * _pow_recursion(x, y - 1));
 }
