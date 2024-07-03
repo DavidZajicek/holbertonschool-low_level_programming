2edb8c484e51f5af982f29f3118a19c2e3fcc1b1
David Zajicek <david.a.zajicek@outlook.com>
2024-07-03 04:16:05 +0000

    I feel like I missed something...

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,6 +1,21 @@
 #include "main.h"
 
 /**
+ * _find_sqrt - Find the square root of a given number
+ * @i: incrementing value
+ * @n: given number to test
+ * Return: result
+ */
+int _find_sqrt(int i, int n)
+{
+	if (i * i == n)
+		return (i);
+	if (i * i > n)
+		return (-1);
+	return (_find_sqrt(i + 1, n));
+}
+
+/**
  * _find_prime - do the logic
  * @i: starting number (2)
  * @sqrt: sqrt of the number to check
@@ -23,7 +38,7 @@ int _find_prime(int i, int sqrt, int n)
  */
 int is_prime_number(int n)
 {
-	int sqrt = _sqrt_recursion(n);
+	int sqrt = _find_sqrt(1, n);
 
 	if (n >= 1)
 		return (_find_prime(2, sqrt, n));
