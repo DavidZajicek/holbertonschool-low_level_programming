7a53c5e51fee0cd807337dff806a7e3ae9b93773
David Zajicek <david.a.zajicek@outlook.com>
2024-07-03 02:40:13 +0000

    4-pow_recursion.c

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+/**
+ * _pow_recursion - return x pow y
+ * @x: number to raise
+ * @y: number to increase x by
+ * Return: result
+ */
+int _pow_recursion(int x, int y)
+{
+	if (x == 0)
+		return (0);
+	if (y < 0)
+		return (-1);
+	if (y > 0)
+		return (_pow_recursion((x * x), y - 1));
+	if (y == 0)
+		return (x);
+}
