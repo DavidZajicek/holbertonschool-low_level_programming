617fdd9543c3ec8ac6b25ea34b3c04986c7a291a
David Zajicek <david.a.zajicek@outlook.com>
2024-07-03 02:22:26 +0000

    2-strlen_recursion.c

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+/**
+ * _strlen_recursion - return the length of a string
+ * @s: string to count
+ * Return: length of string provided
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+}
--- recursion/README.md
+++ recursion/README.md
@@ -3,3 +3,4 @@
 
 0-puts_recursion.c - print a string, followed by a new line
 1-print_rev_recursion.c - print a string in reverse
+2-strlen_recursion.c - return the length of a string
