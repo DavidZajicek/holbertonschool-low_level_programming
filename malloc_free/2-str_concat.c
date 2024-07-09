fa09ecc0c20e86f5d16ef9e7a5b3abe382eb74a5
David Zajicek <david.a.zajicek@outlook.com>
2024-07-09 10:16:05 +0000

    Egan was right again

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -29,9 +29,9 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i = 0;
 
 	if (s1 == NULL)
-		*s1 = "";
+		s1 = "";
 	if (s2 == NULL)
-		*s2 = "";
+		s2 = "";
 
 	i = walk_string(s1) + walk_string(s2);
 
