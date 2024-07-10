0f17b33896d9e6c1bee198c7085c1b5bcfb891b4
David Zajicek <david.a.zajicek@outlook.com>
2024-07-10 23:45:56 +0000

    pointersss

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -10,9 +10,9 @@ void free_dog(dog_t *d)
 {
 	if (d == NULL)
 		return;
-	if (d.owner != NULL)
-		free(d.owner);
-	if (d.name != NULL)
-		free(d.name);
+	if (d->owner != NULL)
+		free(d->owner);
+	if (d->name != NULL)
+		free(d->name);
 	free(d);
 }
