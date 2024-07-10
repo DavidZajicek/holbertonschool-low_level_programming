7df5e24f53ec65232581f41ddd9794a801e49bcf
David Zajicek <david.a.zajicek@outlook.com>
2024-07-10 20:48:51 +0000

    1-string_nconcat.c

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - concat two strings
+ * @s1: string one
+ * @s2: string two
+ * @n: amount of bytes of s2 to add to s1
+ * Return: pointer to the  the start of s1
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	int i = 0, j = 0;
+	char *result = malloc(sizeof(char) * 1024);
+
+	if (result == NULL)
+		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (*s1 != '\0')
+	{
+		result[j] = *s1;
+		s1++;
+		j++;
+	}
+	for (i = 0; i < n; i++)
+	{
+		if (s2[i] == '\0')
+			break;
+		result[j] = s2[i];
+		j++;
+	}
+	result[j] = '\0';
+	return (result);
+}
--- more_malloc_free/README.md
+++ more_malloc_free/README.md
@@ -1,3 +1,4 @@
 # C - More malloc, free
 
-0-malloc_checked.c - use malloc to allocate memory
+0-malloc_checked.c - use malloc to allocate memory  
+1-string_nconcat.c - concat two strings
