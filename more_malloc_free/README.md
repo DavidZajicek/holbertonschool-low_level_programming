706143b19609869c1d6d3ffb04eb6e5a7cf53658
David Zajicek <david.a.zajicek@outlook.com>
2024-07-10 20:34:57 +0000

    0-malloc_checked.c

---
+++ more_malloc_free/README.md
@@ -0,0 +1,3 @@
+# C - More malloc, free
+
+0-malloc_checked.c - use malloc to allocate memory
---
+++ more_malloc_free/main.h
@@ -0,0 +1,6 @@
+#ifndef MoreMalloc
+#define MoreMalloc
+
+void *malloc_checked(unsigned int b);
+
+#endif
