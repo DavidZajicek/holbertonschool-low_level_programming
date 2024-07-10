9ac14a232d559735e041f596e9cc018333b5d697
David Zajicek <david.a.zajicek@outlook.com>
2024-07-10 22:24:47 +0000

    1-init_dog.c

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog - initialize dog
+ * @d: dog struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Return: Void.
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
--- structures_typedef/README.md
+++ structures_typedef/README.md
@@ -1,3 +1,4 @@
 #C - Structures and typedef
 
 dog.h - define a new struct type for dog
+1-init_dog.c - initialize struct dog
