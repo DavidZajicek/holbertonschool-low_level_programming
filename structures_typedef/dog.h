ac59f1b3ab858d93728b0914c9f3c151a0c8b3c1
David Zajicek <david.a.zajicek@outlook.com>
2024-07-10 22:04:05 +0000

    typedef required I guess?

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,11 +1,19 @@
 #ifndef dog
 #define dog
 
-struct dog
+/**
+ * struct dog - dog
+ * @name: string name of the dog
+ * @age: float age of the dog
+ * @owner: String of the owner name
+ *
+ * Description: a dog
+ */
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog;
 
 #endif
