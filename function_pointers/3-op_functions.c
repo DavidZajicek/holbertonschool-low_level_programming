991b271447672660bfa3bb2de95ffd8869ecdf24
David Zajicek <david.a.zajicek@outlook.com>
2024-07-15 11:37:39 +0000

    3-calc

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include <stdlib.h>
+#include "3-calc.h"
+
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+		return (-1);
+	return (a / b);
+}
+
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+		return (-1);
+	return (a % b);
+}
--- function_pointers/README.md
+++ function_pointers/README.md
@@ -2,4 +2,5 @@
 
 0-print_name.c - print a name  
 1-array_iterator.c - execute an action on each element of a given array  
-2-int_index.c - search for a given int
+2-int_index.c - search for a given int  
+3-main.c - it does a lot of things apparently
