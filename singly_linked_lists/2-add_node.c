511fe2795e81517ec8574b66f06ec4608920444f
David Zajicek <david.a.zajicek@outlook.com>
2024-07-30 10:03:21 +0000

    forgot about if malloc fails

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -12,6 +12,11 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_head = malloc(sizeof(list_t));
 
+	if (new_head == NULL)
+	{
+		free(new_head);
+		return (NULL);
+	}
 	new_head->str = strdup(str);
 	new_head->next = head;
 
