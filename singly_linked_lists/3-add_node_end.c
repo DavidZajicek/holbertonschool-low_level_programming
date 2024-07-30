362ac8454f1e01383d5ade7ad4e89fef463d78a1
David Zajicek <david.a.zajicek@outlook.com>
2024-07-30 11:07:39 +0000

    I miss my autocomplete

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -15,9 +15,9 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new_node = (list_t *)malloc(sizeof(list_t));
 	int count = 0;
 
-	while (current->next != NULL)
+	while (current_node->next != NULL)
 	{
-		current = current->next;
+		current_node = current_node->next;
 	}
 
 	if (new_node == NULL)
@@ -38,6 +38,6 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node->len = count;
 	new_node->next = NULL;
 
-	current->next = new_node;
+	current_node->next = new_node;
 	return (*head);
 }
