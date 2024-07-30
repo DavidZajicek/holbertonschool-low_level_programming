f888b6f5af0433f6d982759be7b50c3507168732
David Zajicek <david.a.zajicek@outlook.com>
2024-07-30 09:34:05 +0000

    0-print_list.c

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stddef.h>
+#include "lists.h"
+
+/**
+ * print_list - print singly linked list
+ * @head: Linked list head (first node)
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *head)
+{
+	list_t *current = head;
+	size_t count = 0;
+
+	while (current != NULL)
+	{
+		count++;
+		if (current->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%i] %s\n", current->len, current->str);
+		current = current->next;
+	}
+	return (count);
+}
