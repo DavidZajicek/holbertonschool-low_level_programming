8b0ec9a9f5b422227e103347c46fe8d2ffd5af35
David Zajicek <david.a.zajicek@outlook.com>
2024-07-31 03:22:20 +0000

    7-insert_dnodeint.c

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include "lists.h"
+
+
+/**
+ * insert_dnodeint_at_index - insert node
+ * @head: head of list
+ * @index: index to add new node
+ * @n: value for new node
+ * Return: address of new node
+ */
+dlistint_t *insert_dnodeint_at_index(
+	dlistint_t **head, unisgned int index, int n)
+{
+	dlistint_t *current_node = *head, *new_node;
+	int count = 0;
+
+	if (index == 0)
+	{
+		current_node = add_dnodeint(head, n);
+		*head = current_node;
+		return (current_node);
+	}
+	if (*head == NULL)
+	{
+		return (NULL);
+	}
+
+	while (count <= index)
+	{
+		if (current_node == NULL && count != index)
+		{
+			return (NULL);
+		}
+		if (count == index)
+		{
+			new_node = create_dnode(n);
+			new_node->prev = current_node->prev;
+			new_node->next = current_node;
+			current_node->next = new_node;
+			return (new_node);
+		}
+		count++;
+		current_node = current_node->next;
+	}
+	return (NULL);
+}
