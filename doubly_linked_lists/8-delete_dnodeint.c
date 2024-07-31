28fbf5df6fdcc4b064401fa58d13496cffcea4f1
David Zajicek <david.a.zajicek@outlook.com>
2024-07-31 04:09:52 +0000

    unsignd

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -10,7 +10,7 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *node = *head;
-	int count = 0;
+	unsigned int count = 0;
 
 	while (count < index && node != NULL)
 	{
