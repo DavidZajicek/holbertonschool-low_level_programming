065ef18e09b0bc299cfcab85f81960f114a6f11e
David Zajicek <david.a.zajicek@outlook.com>
2024-07-31 01:27:09 +0000

    3-add_dnodeint_end.c

---
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * create_dnode - create dnode
+ * @n: n
+ * Return: address of new node
+ */
+dlistint_t *create_dnode(const int n)
+{
+	dlistint_t *new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = NULL;
+	return (new_node);
+}
+
+/**
+ * add_dnodeint_end - add node to end of dll
+ * @head: head of list
+ * @n: n
+ * Return: new element's address
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node, *current_node = *head;
+
+	new_node = create_dnode(n);
+	if (new_node == NULL)
+		return (NULL);
+
+	if (*head != NULL)
+	{
+		while (current_node->next != NULL)
+			current_node = current_node->next;
+
+		new_node->prev = current_node;
+		current_node->next = new_node;
+	}
+
+
+	return (new_node);
+}
