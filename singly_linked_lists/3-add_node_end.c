a5e43aa127cb591c5beafe3f606660c7787109db
David Zajicek <david.a.zajicek@outlook.com>
2024-07-31 00:15:20 +0000

    create_node

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -4,21 +4,13 @@
 #include "lists.h"
 
 /**
- * add_node_end - add node to end of linked list
- * @head: start of linked list
- * @str: string to add into the linked list
- * Return: starting node
+ * create_node - create node
+ * @str: string
+ * Return: new node
  */
-list_t *add_node_end(list_t **head, const char *str)
+list_t create_node(const char *str)
 {
-	list_t *current_node = *head;
 	list_t *new_node = (list_t *)malloc(sizeof(list_t));
-	int count = 0;
-
-	while (current_node->next != NULL)
-	{
-		current_node = current_node->next;
-	}
 
 	if (new_node == NULL)
 	{
@@ -37,6 +29,35 @@ list_t *add_node_end(list_t **head, const char *str)
 	}
 	new_node->len = count;
 	new_node->next = NULL;
+
+	return (new_node);
+}
+
+/**
+ * add_node_end - add node to end of linked list
+ * @head: start of linked list
+ * @str: string to add into the linked list
+ * Return: starting node
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *current_node = *head;
+	list_t *new_node;
+	int count = 0;
+
+	new_node = create_node(new_node);
+	if (new_node == NULL)
+		return (NULL);
+
+	if (current_node == NULL)
+	{
+		*head = current_node;
+		return (new_node);
+	}
+	while (current_node->next != NULL)
+	{
+		current_node = current_node->next;
+	}
 
 	current_node->next = new_node;
 
