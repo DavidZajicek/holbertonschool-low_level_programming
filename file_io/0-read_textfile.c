a2f85ecbc7c5814c98ae62593bf5dc92eff2ee7d
David Zajicek <david.a.zajicek@outlook.com>
2024-08-04 09:01:33 +0000

    start

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+#include <stddef.h>
+
+/**
+ * read_textfile - read text file
+ * @filename: file to read
+ * @letters: number of letters to read from file
+ * Return: number of letters read or 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+}
