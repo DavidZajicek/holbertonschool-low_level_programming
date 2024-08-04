5087a89451567a1610722ce537e5f85aa0229ffd
David Zajicek <david.a.zajicek@outlook.com>
2024-08-04 03:16:12 +0000

    4-clear_bit.c

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * clear_bit - clear bit
+ * @n: number
+ * @index: bit to clear
+ * Return: success or failure
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+
+	*n &= (0UL << index);
+	return (1);
+}
