428586ed736d3a7c85f8e293b3858e702a3a4732
David Zajicek <david.a.zajicek@outlook.com>
2024-08-04 03:04:46 +0000

    charbit

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -9,7 +9,7 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= sizeof(unsigned long int) * CHAR_BIT)
+	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 	return ((n >> index) & 1);
 }
