da27a126e61e185fb0fd4cf90fc931f05f17fbf2
David Zajicek <david.a.zajicek@outlook.com>
2024-08-04 02:33:04 +0000

    unsigned signed

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -9,6 +9,7 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int result = 0, i = 0, pow = 1;
+	int j = 0;
 
 	if (b == NULL)
 		return (0);
@@ -19,9 +20,9 @@ unsigned int binary_to_uint(const char *b)
 		else
 			return (0);
 	}
-	for (i = i - 1; i >= 0; i--)
+	for (j = i - 1; j >= 0; j--)
 	{
-		if (b[i] == '1')
+		if (b[j] == '1')
 			result += pow;
 		pow *= 2;
 	}
