81cf5c0cdded8f554094bb1d5f17e97d84d5b023
David Zajicek <david.a.zajicek@outlook.com>
2024-08-05 10:29:40 +0000

    malloc?

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -11,17 +11,34 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int n, fd;
-	char buf[50];
+	int fd;
+	ssize_t n;
+	char *buf;
 
-	if (filename == NULL)
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
 		return (0);
+	if (filename == NULL)
+	{
+		free(buf);
+		return (0);
+	}
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
+	{
+		free(buf);
 		return (0);
+	}
 	n = read(fd, buf, letters);
 	if (n == -1)
+	{
+		free(buf);
+		close(fd);
 		return (0);
+	}
+
 	write(1, buf, n);
+	free(buf);
+	close(fd);
 	return (n);
 }
