5ca99ac0a66dd47217578746c0fa526926ac91d6
David Zajicek <david.a.zajicek@outlook.com>
2024-08-05 10:49:55 +0000

    0500

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -17,13 +17,13 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_WRONLY | O_TRUNC, S_IREAD | S_IWRITE);
+	fd = open(filename, O_WRONLY | O_TRUNC, 0500);
 	if (fd == -1)
 	{
 		return (-1);
 	}
 
-	write(fd, test_content);
+	write(fd, text_content);
 	close(fd);
 	return (1);
 }
