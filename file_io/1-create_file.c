e93817577baddf8091cffd588263e1300d8cc8af
David Zajicek <david.a.zajicek@outlook.com>
2024-08-05 11:10:35 +0000

    jhust check before pushing dabvid

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0300);
+	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
 	if (fd == -1)
 	{
 		return (-1);
