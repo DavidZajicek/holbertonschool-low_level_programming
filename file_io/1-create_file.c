33b87c983122cce8a1b782174e0b15e531c01e45
David Zajicek <david.a.zajicek@outlook.com>
2024-08-05 11:13:02 +0000

    just don't wri9te them

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -26,9 +26,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	if (text_content == NULL)
-		write(fd, "", len);
-	else
+	if (text_content != NULL)
 		write(fd, text_content, len);
 	close(fd);
 	return (1);
