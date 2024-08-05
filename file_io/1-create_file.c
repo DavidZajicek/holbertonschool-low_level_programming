0d2d5c37e754da233c4e147946cdf2afe36f609b
David Zajicek <david.a.zajicek@outlook.com>
2024-08-05 11:05:53 +0000

    maybe... create the file???

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_RDWR | O_TRUNC, 0500);
+	fd = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0500);
 	if (fd == -1)
 	{
 		return (-1);
