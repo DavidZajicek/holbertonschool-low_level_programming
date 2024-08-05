74e4ede10c565066acd0a697a637395dca7b0f4a
David Zajicek <david.a.zajicek@outlook.com>
2024-08-05 11:18:36 +0000

    null

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -15,7 +15,8 @@ int create_file(const char *filename, char *text_content)
 	int fd;
 	size_t len;
 
-	len = strlen(text_content);
+	if (text_content != NULL)
+		len = strlen(text_content);
 	if (filename == NULL)
 	{
 		return (-1);
