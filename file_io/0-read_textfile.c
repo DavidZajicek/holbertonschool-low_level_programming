576683c3e7dfc072bb1d4e23e5ac18d91569e0e1
David Zajicek <david.a.zajicek@outlook.com>
2024-08-05 09:56:40 +0000

    0-read_textfile.c

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <unistd.h>
 #include <stddef.h>
+#include <fcntl.h>
 
 /**
  * read_textfile - read text file
@@ -10,4 +11,17 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	int n, input_file;
+	char buffer[50];
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	n = read(fd, buf, letters);
+	if (n == -1)
+		return (0);
+	write(1, buf, n);
+	return (n);
 }
